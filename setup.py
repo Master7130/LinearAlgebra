3e0d7cdb8fd3a885430378d7376798c2625ab41f
Master7130 <mandalapubhavesh@gmail.com>
2021-06-15 19:58:17 -0500

    Initial Commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='mypythonlib',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.2.0',
     description='My first Python library',
     author='Bhavesh Mandalapu',
     author_email="mandalapubhavesh@gmail.com",
